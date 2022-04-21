e8349fad32d2e7330ffeb336eb94737c6d3c656e
amir58 <devamir11@gmail.com>
2022-04-21 23:02:47 +0200

    Add image

--- app/src/main/java/com/example/inhomecareapp/caregiver/CaregiverRegisterTabFragment.java
+++ app/src/main/java/com/example/inhomecareapp/caregiver/CaregiverRegisterTabFragment.java
@@ -1,6 +1,8 @@
 package com.example.inhomecareapp.caregiver;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,14 +12,18 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.inhomecareapp.R;
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
@@ -28,10 +34,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
@@ -44,6 +47,7 @@ public class CaregiverRegisterTabFragment extends Fragment {
     EditText caregiverIdEt;
     EditText caregiverEmailRegisterEt;
     EditText caregiverPhoneRegisterEt;
+    ImageView imageViewPic;
     MaterialButton caregiverRegisterBtn;
     RadioButton radioButton1;
     RadioButton radioButton2;
@@ -53,16 +57,29 @@ public class CaregiverRegisterTabFragment extends Fragment {
     private String caregiverNameRegister, caregiverEmailRegister, caregiverPhoneRegister;
 
     @Override
-    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.caregiver_register_fragment,container,false);
-        caregiverNameRegisterEt=root.findViewById(R.id.caregiverName_register);
-        caregiverPassRegisterEt=root.findViewById(R.id.caregiverPass_register);
-        caregiverIdEt=root.findViewById(R.id.caregiverID_register);
-        caregiverEmailRegisterEt=root.findViewById(R.id.caregiverEmail_register);
-        caregiverPhoneRegisterEt=root.findViewById(R.id.caregiverPhone_register);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.caregiver_register_fragment, container, false);
+        imageViewPic = root.findViewById(R.id.caregiver_profile_Pic_register);
+        caregiverNameRegisterEt = root.findViewById(R.id.caregiverName_register);
+        caregiverPassRegisterEt = root.findViewById(R.id.caregiverPass_register);
+        caregiverIdEt = root.findViewById(R.id.caregiverID_register);
+        caregiverEmailRegisterEt = root.findViewById(R.id.caregiverEmail_register);
+        caregiverPhoneRegisterEt = root.findViewById(R.id.caregiverPhone_register);
         radioButton1 = root.findViewById(R.id.radio_button_1);
         radioButton2 = root.findViewById(R.id.radio_button_2);
         caregiverRegisterBtn = root.findViewById(R.id.caregiverRegister_btn);
+
+        imageViewPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImagePicker.with(CaregiverRegisterTabFragment.this)
+                        .crop()                    //Crop image(Optional), Check Customization for more option
+                        .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+
         caregiverRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,12 +90,11 @@ public class CaregiverRegisterTabFragment extends Fragment {
 
 
                 if (caregiverNameRegister.isEmpty() || caregiverEmailRegister.isEmpty() || caregiverPassRegister.isEmpty() ||
-                        caregiverPhoneRegister.isEmpty() ) {
+                        caregiverPhoneRegister.isEmpty()) {
                     Toast.makeText(requireContext(), "please fill all data", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (caregiverPassRegister.length() < 6)
-                {
+                if (caregiverPassRegister.length() < 6) {
                     Toast.makeText(requireContext(), "password should be at least 6 characters", Toast.LENGTH_SHORT).show();
                     return;
                 }
@@ -89,6 +105,24 @@ public class CaregiverRegisterTabFragment extends Fragment {
         });
         return root;
 
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i(TAG, "onActivityResult: ");
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            Uri uri = data.getData();
+            Log.i(TAG, "onActivityResult: " + uri);
+            // Use Uri object instead of File to avoid storage permissions
+            imageViewPic.setImageURI(uri);
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(requireContext(), ImagePicker.getError(data), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(requireContext(), "Task Cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void createCaregiverByEmail(String caregiverEmailRegister, String caregiverPassRegister) {
@@ -103,7 +137,7 @@ public class CaregiverRegisterTabFragment extends Fragment {
                         } else {
                             String errorMessage = task.getException().getLocalizedMessage();
                             Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show();
-                            Log.i(TAG,"onComplete: "+errorMessage);
+                            Log.i(TAG, "onComplete: " + errorMessage);
                         }
 
                     }
@@ -125,7 +159,7 @@ public class CaregiverRegisterTabFragment extends Fragment {
                         if (task.isSuccessful()) {
                             Toast.makeText(requireContext(), "User data is uploaded", Toast.LENGTH_SHORT).show();
                             Log.i(TAG, "onComplete: User data uploaded");
-                            Intent intent=new Intent(requireContext(), CaregiverHome.class);
+                            Intent intent = new Intent(requireContext(), CaregiverHome.class);
                             startActivity(intent);
                         } else {
                             String errorMessage = task.getException().getLocalizedMessage();
@@ -137,6 +171,7 @@ public class CaregiverRegisterTabFragment extends Fragment {
 
 
     }
+
     private ArrayList<String> getCustomerList() {
         ArrayList<String> customers = new ArrayList<>();
         customers.add("Hourly Services");
@@ -144,6 +179,7 @@ public class CaregiverRegisterTabFragment extends Fragment {
 
         return customers;
     }
+
     private ArrayList<String> getCcategoryList() {
         ArrayList<String> customers = new ArrayList<>();
         customers.add("Elderly");
@@ -154,9 +190,7 @@ public class CaregiverRegisterTabFragment extends Fragment {
     }
 
 
-
-    private void sendVerificationEmail()
-    {
+    private void sendVerificationEmail() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         user.sendEmailVerification()
