20edf49f88c774f8df09c6542ef82f22b0bb120d
amir58 <devamir11@gmail.com>
2022-04-28 16:14:36 +0200

    Finish add post

---
+++ app/src/main/java/com/example/inhomecareapp/CustomerPost.java
@@ -0,0 +1,56 @@
+package com.example.inhomecareapp;
+
+public class CustomerPost {
+
+    private String UserId;
+    private String postId;
+    private String postContent;
+    private boolean accept = false;
+
+    public CustomerPost(String userId, String postId, String postContent) {
+        UserId = userId;
+        this.postId = postId;
+        this.postContent = postContent;
+    }
+
+    public boolean isAccept() {
+        return accept;
+    }
+
+    public void setAccept(boolean accept) {
+        this.accept = accept;
+    }
+
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getPostContent() {
+        return postContent;
+    }
+
+    public void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerPost{" +
+                "UserId='" + UserId + '\'' +
+                ", postId='" + postId + '\'' +
+                ", postContent='" + postContent + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/com/example/inhomecareapp/customer/CustomerHome.java
+++ app/src/main/java/com/example/inhomecareapp/customer/CustomerHome.java
@@ -12,59 +12,64 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.inhomecareapp.CustomerPost;
 import com.example.inhomecareapp.R;
 import com.example.inhomecareapp.caregiver.CaregiverHome;
 import com.example.inhomecareapp.caregiver.CaregiverProfileActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CustomerHome extends AppCompatActivity {
-     String[] servicesTypes={"Hourly service","Stay in service"};
-     AutoCompleteTextView autoCompleteServiceType;
-     ArrayAdapter<String> adapterServiceTypeItems;
+    String[] servicesTypes = {"Hourly service", "Stay in service"};
+    AutoCompleteTextView autoCompleteServiceType;
+    ArrayAdapter<String> adapterServiceTypeItems;
 
-    String[] categories={"Children","Elderly","People with special needs"};
+    String[] categories = {"Children", "Elderly", "People with special needs"};
     AutoCompleteTextView autocompleteCategories;
     ArrayAdapter<String> adapterCategoriesItems;
 
-    String[] ages={"0 - 3 months","3 months - 4 years" ,"10 - 20 years" ,"20 - 40 years" ,"40 - 80 years" };
+    String[] ages = {"0 - 3 months", "3 months - 4 years", "10 - 20 years", "20 - 40 years", "40 - 80 years"};
     AutoCompleteTextView autoCompleteTAges;
     ArrayAdapter<String> adapterAgesItems;
 
     FloatingActionButton addPostBtn;
     AlertDialog dialogAddPost;
     MaterialButton findCaregiverBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_home);
-        findCaregiverBtn=findViewById(R.id.find_caregiverBtn);
+        findCaregiverBtn = findViewById(R.id.find_caregiverBtn);
         findCaregiverBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(CustomerHome.this, CaregiverListActivity.class);
+                Intent intent = new Intent(CustomerHome.this, CaregiverListActivity.class);
                 startActivity(intent);
             }
         });
 
 
-
-        autoCompleteServiceType=findViewById(R.id.autoComplete_service_type);
-        adapterServiceTypeItems= new ArrayAdapter<String>(this,R.layout.list_item_service_type,servicesTypes);
+        autoCompleteServiceType = findViewById(R.id.autoComplete_service_type);
+        adapterServiceTypeItems = new ArrayAdapter<String>(this, R.layout.list_item_service_type, servicesTypes);
         autoCompleteServiceType.setAdapter(adapterServiceTypeItems);
         autoCompleteServiceType.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                 adapterServiceTypeItems.getItem(position);
+                adapterServiceTypeItems.getItem(position);
 
             }
         });
 
-        autocompleteCategories=findViewById(R.id.autoComplete_select_category);
-        adapterCategoriesItems =new ArrayAdapter<String>(this,R.layout.list_item_select_category,categories);
+        autocompleteCategories = findViewById(R.id.autoComplete_select_category);
+        adapterCategoriesItems = new ArrayAdapter<String>(this, R.layout.list_item_select_category, categories);
         autocompleteCategories.setAdapter(adapterCategoriesItems);
         autoCompleteServiceType.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -73,8 +78,8 @@ public class CustomerHome extends AppCompatActivity {
             }
         });
 
-        autoCompleteTAges=findViewById(R.id.autoComplete_ages);
-        adapterAgesItems= new ArrayAdapter<String>(this,R.layout.list_item_ages,ages);
+        autoCompleteTAges = findViewById(R.id.autoComplete_ages);
+        adapterAgesItems = new ArrayAdapter<String>(this, R.layout.list_item_ages, ages);
         autoCompleteTAges.setAdapter(adapterAgesItems);
         autoCompleteTAges.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -83,21 +88,52 @@ public class CustomerHome extends AppCompatActivity {
 
             }
         });
-        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Create new post");
-        View view = getLayoutInflater().inflate(R.layout.dialog_add_post,null);
+        View view = getLayoutInflater().inflate(R.layout.dialog_add_post, null);
         TextInputEditText writePostEt;
         Button submitPostBtn;
-        writePostEt=findViewById(R.id.write_post_et);
-        submitPostBtn=findViewById(R.id.submit_post_btn);
+        writePostEt = view.findViewById(R.id.write_post_et);
+        submitPostBtn = view.findViewById(R.id.submit_post_btn);
+
+        submitPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String content = writePostEt.getText().toString();
+
+                if (content.isEmpty()) {
+                    Toast.makeText(CustomerHome.this, "content required", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                CustomerPost customerPost = new CustomerPost(
+                        FirebaseAuth.getInstance().getUid(),
+                        String.valueOf(System.currentTimeMillis()),
+                        content
+                );
+
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+                firestore.collection("posts").document(customerPost.getPostId())
+                        .set(customerPost).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        writePostEt.setText("");
+                        Toast.makeText(CustomerHome.this, "Post added", Toast.LENGTH_SHORT).show();
+                        dialogAddPost.dismiss();
+                    }
+                });
+
+            }
+        });
 
         builder.setView(view);
-        dialogAddPost=builder.create();
-        addPostBtn=findViewById(R.id.add_post_btn);
+        dialogAddPost = builder.create();
+        addPostBtn = findViewById(R.id.add_post_btn);
         addPostBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              dialogAddPost.show();
+                dialogAddPost.show();
             }
         });
 
@@ -113,7 +149,6 @@ public class CustomerHome extends AppCompatActivity {
                 }
                 return true;
             }
-
 
 
         });
